c23cb7334fd4f346076cd5eb42f08b9826838a88
rastogi-neha <rastogi.neh@northeastern.edu>
2023-02-18 23:16:43 -0500

    Assignment 5 : Fixes made to Main.java in order to perform threading to check how effective Parallel sorting is

--- src/main/java/edu/neu/coe/info6205/sort/par/Main.java
+++ src/main/java/edu/neu/coe/info6205/sort/par/Main.java
@@ -20,24 +20,34 @@ public class Main {
         processArgs(args);
         System.out.println("Degree of parallelism: " + ForkJoinPool.getCommonPoolParallelism());
         Random random = new Random();
-        int[] array = new int[2000000];
+        //int[] array = new int[2000000];
         ArrayList<Long> timeList = new ArrayList<>();
-        for (int j = 50; j < 100; j++) {
-            ParSort.cutoff = 10000 * (j + 1);
-            // for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
-            long time;
-            long startTime = System.currentTimeMillis();
-            for (int t = 0; t < 10; t++) {
-                for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
-                ParSort.sort(array, 0, array.length);
+        int[] array;
+        for(int sizeOfArray=1500000; sizeOfArray<=3000000; sizeOfArray+=500000) {
+            System.out.println("Array size ：" + sizeOfArray);
+            array = new int[sizeOfArray];
+            for (int threadCount = 2; threadCount < 65; threadCount = threadCount * 2) {
+                ForkJoinPool myPool = new ForkJoinPool(threadCount);
+                System.out.println("Thread count is: " + threadCount);
+                for (int j = 0; j < 100; j++) {
+                    ParSort.cutoff = 20000 * (j + 1);
+                    // for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
+                    long time;
+                    long startTime = System.currentTimeMillis();
+                    for (int t = 0; t < 10; t++) {
+                        for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
+                        ParSort.sort(array, 0, array.length);
+                    }
+                    long endTime = System.currentTimeMillis();
+                    time = (endTime - startTime);
+                    timeList.add(time);
+
+
+                    //System.out.println("cutoff：" + (ParSort.cutoff) + "\t\t10times Time:" + time + "ms");
+                    System.out.println(time);
+
+                }
             }
-            long endTime = System.currentTimeMillis();
-            time = (endTime - startTime);
-            timeList.add(time);
-
-
-            System.out.println("cutoff：" + (ParSort.cutoff) + "\t\t10times Time:" + time + "ms");
-
         }
         try {
             FileOutputStream fis = new FileOutputStream("./src/result.csv");
